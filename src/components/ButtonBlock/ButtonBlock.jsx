24c554b6bc11bd787cf609c5050304c64ed3c593
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-02-19 20:09:38 +0200

    finish progress bar

--- src/components/ButtonBlock/ButtonBlock.jsx
+++ src/components/ButtonBlock/ButtonBlock.jsx
@@ -1,10 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import "./ButtonBlock.css";
 
 const ButtonsBlock = ({ handleButtonClick }) => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (index) => {
+    handleButtonClick(index);
+    setActiveButton(index);
+  };
+
   return (
     <div className='buttons-block'>
       {[...Array(10).keys()].map((index) => (
-        <button key={index + 1} onClick={() => handleButtonClick(index)}>
+        <button
+          key={index + 1}
+          onClick={() => handleClick(index)}
+          className={`progress-btn ${index === 0 ? "first-btn" : ""} ${
+            index === 9 ? "last-btn" : ""
+          } ${
+            activeButton !== null && index <= activeButton ? "active-btn" : ""
+          }`}
+        >
           {index + 1}
         </button>
       ))}
